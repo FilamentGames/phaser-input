033b6234ee7343101611e54e31bef64f43361b6a
Alex Stone <stone@filamentgames.com>
2016-08-23 22:02:52 -0500

    Declare phaser-input module

--- ts/phaser-input-shim.d.ts
+++ ts/phaser-input-shim.d.ts
@@ -1,3 +1,7 @@
+declare module "phaser-input" {
+	export = Fabrique;
+}
+
 declare module Phaser {
 	interface GameObjectFactory {
 		inputField(x:number, y:number, inputOptions?:Fabrique.InputOptions, group?:Phaser.Group):Fabrique.InputField;
