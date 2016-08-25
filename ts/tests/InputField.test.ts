1d91078d3238b4e1e289b98997a0d30cf033162b
Alex Stone <stone@filamentgames.com>
2016-08-25 11:24:18 -0500

    wordWrapWidth option is not actually used. Instead it's using width.

--- ts/tests/InputField.test.ts
+++ ts/tests/InputField.test.ts
@@ -23,4 +23,29 @@ describe("InputField", () => {
             }
         }, true);
     }, 10000);
+    it("can display a multiline input field", (cb) => {
+        var game = new Phaser.Game({
+            width: "100%",
+            height: "100%"
+        });
+        (<any>window).game = game;
+        game.state.add("test", {
+            init: function () {
+                game.plugins.add(Fabrique.Plugins.InputField);
+            },
+            create: function () {
+                var input = game.add.inputField(50, 50, {
+                    width: 250,
+                    placeHolder: "Enter text here",
+                    height: 250,
+                    wordWrap: true
+                });
+
+                setTimeout(function () {
+                    game.destroy();
+                    cb();
+                }, 5000);
+            }
+        }, true);
+    }, 10000);
 });
